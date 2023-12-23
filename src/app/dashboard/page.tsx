908f27bdd8f41ac73225ca3e4e1cf7748d8fce74
schandillia <amit@schandillia.com>
2023-12-23 19:57:42 +0530

    Fixed Kinde API user data retrieval

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,16 @@
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server"
 
-const Page = () => {
-    const {getUser} = getKindeServerSession()
-    const user = getUser()
+// const Page = () => {
+//     const {getUser} = getKindeServerSession()
+//     const user = getUser()
 
-    return <div>Hello {user.email}</div>
-}
+//     return <div>Hello {user.givenName}</div>
+// }
+
+const Page = async () => {
+    const {getUser} = getKindeServerSession();
+    const user = await getUser();
+    return <p>Hi {user?.given_name} {user?.family_name}!</p>;
+};
+
 export default Page
