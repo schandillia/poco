df8bb554fa28510c13f3954f56ed8aab5acf5c5c
schandillia <amit@schandillia.com>
2023-12-26 08:13:53 +0530

    Fixed upload button dialog

--- src/components/UploadButton.tsx
+++ src/components/UploadButton.tsx
@@ -1,27 +1,48 @@
 "use client"
-import {
-    Dialog,
-    DialogContent,
-    DialogTrigger
-} from "@radix-ui/react-dialog"
-import { Button } from "./ui/button"
-import { useState } from "react"
 
-const UploadButton = () => {
-    const [isOpen, setIsOpen] = useState<boolean>(false)
-    return (
-        <Dialog
-            open={isOpen}
-            onOpenChange={(v) => {
-                if (!v) {
-                    setIsOpen(v)
-                }
-            }}>
-            <DialogTrigger onClick={() => setIsOpen(true)} asChild>
-                <Button>Upload PDF</Button>
-            </DialogTrigger>
-            <DialogContent>example</DialogContent>
-        </Dialog>
-    )
+import { useState } from "react"
+import {
+  Dialog,
+  DialogContent,
+  DialogTrigger,
+} from "./ui/dialog"
+import { Button } from "./ui/button"
+
+// import Dropzone from "react-dropzone"
+// import { Cloud, File, Loader2 } from "lucide-react"
+// import { Progress } from "./ui/progress"
+// import { useUploadThing } from "@/lib/uploadthing"
+// import { useToast } from "./ui/use-toast"
+import { trpc } from "@/app/_trpc/client"
+import { useRouter } from "next/navigation"
+
+
+const UploadButton = ({
+  isSubscribed,
+}: {
+  isSubscribed: boolean
+}) => {
+  const [isOpen, setIsOpen] = useState<boolean>(false)
+
+  return (
+    <Dialog
+      open={isOpen}
+      onOpenChange={(v) => {
+        if (!v) {
+          setIsOpen(v)
+        }
+      }}>
+      <DialogTrigger
+        onClick={() => setIsOpen(true)}
+        asChild>
+        <Button>Upload PDF</Button>
+      </DialogTrigger>
+
+      <DialogContent>
+        <div>Test!!</div>
+      </DialogContent>
+    </Dialog>
+  )
 }
+
 export default UploadButton
