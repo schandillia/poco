f92b216880ce9ecf9e6d3d6aa192e4e4e4a21345
schandillia <amit@schandillia.com>
2023-12-26 22:32:31 +0530

    Added file display page and improved file upload dialog

--- src/components/UploadButton.tsx
+++ src/components/UploadButton.tsx
@@ -4,13 +4,40 @@ import { useState } from "react";
 import { Dialog, DialogContent, DialogTrigger } from "./ui/dialog";
 import { Button } from "./ui/button";
 
-// import Dropzone from "react-dropzone"
+import Dropzone from "react-dropzone"
+
 // import { Cloud, File, Loader2 } from "lucide-react"
 // import { Progress } from "./ui/progress"
 // import { useUploadThing } from "@/lib/uploadthing"
 // import { useToast } from "./ui/use-toast"
 import { trpc } from "@/app/_trpc/client";
 import { useRouter } from "next/navigation";
+
+const UploadDropzone = () => {
+  return (
+    <Dropzone
+      multiple={false}
+      onDrop={(acceptedFile) => {
+        console.log(acceptedFile)
+      }}
+    >
+      {({getRootProps, getInputProps, acceptedFiles}) => (
+        <div {...getRootProps()} className='border h-64 m-4 border-dashed border-gray-300 rounded-lg'>
+          <div className='flex items-center justify-center h-full w-full'>
+            <label
+              htmlFor='dropzone-file'
+              className='flex flex-col items-center justify-center w-full h-full rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100'
+            >
+              <div className='flex flex-col items-center justify-center pt-5 pb-6'>
+                example
+              </div>
+            </label>
+          </div>
+        </div>
+      )}
+    </Dropzone>
+  )
+}
 
 const UploadButton = (
   {
@@ -30,11 +57,11 @@ const UploadButton = (
       }}
     >
       <DialogTrigger onClick={() => setIsOpen(true)} asChild>
-        <Button>Upload PDF</Button>
+        <Button>Upload Paper</Button>
       </DialogTrigger>
 
       <DialogContent>
-        <div>Test!!</div>
+        <UploadDropzone />
       </DialogContent>
     </Dialog>
   );
