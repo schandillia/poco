bf8648e8d95d0d7b1c1d3167a82927b9f1efa49b
schandillia <amit@schandillia.com>
2023-12-27 12:01:53 +0530

    Improved upload progress and added pdf rendering

--- src/components/PdfRenderer.tsx
+++ src/components/PdfRenderer.tsx
@@ -1,8 +1,29 @@
-const PdfRenderer = () => {
+import { Document, Page, pdfjs } from "react-pdf"
+
+import "react-pdf/dist/Page/AnnotationLayer.css"
+import "react-pdf/dist/Page/TextLayer.css"
+
+import { useToast } from "./ui/use-toast"
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`
+
+interface PdfRendererProps {
+    url: string
+}
+
+const PdfRenderer = ({ url }: PdfRendererProps) => {
     return (
         <div className='w-full bg-white rounded-md shadow flex flex-col items-center'>
             <div className='h-14 w-full border-b border-zinc-200 flex items-center justify-between px-2'>
                 <div className='flex items-center gap-1.5'></div>
+            </div>
+
+            <div className='flex-1 w-full max-h-screen'>
+                <div>
+                    <Document file={url} className="max-h-full">
+                        <Page pageNumber={1} />
+                    </Document>
+                </div>
             </div>
         </div>
     )
