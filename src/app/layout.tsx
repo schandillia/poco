da6f25e3416c57380bb5c1e0a5dc20532a60bdfd
schandillia <amit@schandillia.com>
2023-12-27 09:41:19 +0530

    Implemented file upload functionality

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,9 +6,8 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 
 import "react-loading-skeleton/dist/skeleton.css";
+import { Toaster } from "@/components/ui/toaster";
 // import "simplebar-react/dist/simplebar.min.css"
-
-// import { Toaster } from "@/components/ui/toaster"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -28,6 +27,7 @@ export default function RootLayout({
         <body
           className={cn("min-h-screen font-sans antialiased", inter.className)}
         >
+          <Toaster />
           <Navbar />
           {children}
         </body>
