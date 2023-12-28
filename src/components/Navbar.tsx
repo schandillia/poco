d578a27c9e613b3fb75cde5159a75a4741c8f47b
schandillia <amit@schandillia.com>
2023-12-28 19:12:03 +0530

    Added logged-in user functionality and logout link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,20 @@
-import Link from "next/link";
-import MaxWidthWrapper from "./MaxWidthWrapper";
-import { buttonVariants } from "./ui/button";
+
+
+import Link from "next/link"
+import MaxWidthWrapper from "./MaxWidthWrapper"
+import { buttonVariants } from "./ui/button"
 import {
   LoginLink,
   RegisterLink,
   getKindeServerSession,
-} from "@kinde-oss/kinde-auth-nextjs/server";
-import { ArrowRight } from "lucide-react";
-// import UserAccountNav from './UserAccountNav'
-// import MobileNav from './MobileNav'
+} from "@kinde-oss/kinde-auth-nextjs/server"
+import { ArrowRight } from "lucide-react"
+import UserAccountNav from "./UserAccountNav"
+// import MobileNav from "./MobileNav"
 
-const Navbar = () => {
-  // const { getUser } = getKindeServerSession()
-  // const user = getUser()
+const Navbar = async () => {
+  const { getUser } = getKindeServerSession()
+  const user = await getUser()
 
   return (
     <nav className="sticky h-14 inset-x-0 top-0 z-30 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
@@ -25,7 +27,7 @@ const Navbar = () => {
           {/*add mobile navbar*/}
 
           <div className="hidden items-center space-x-4 sm:flex">
-            <>
+            {!user? <>
               <Link
                 href="/pricing"
                 className={buttonVariants({
@@ -50,12 +52,32 @@ const Navbar = () => {
               >
                 Get started <ArrowRight className="ml-1.5 h-5 w-5" />
               </RegisterLink>
-            </>
+            </> : <>
+              <Link
+                href="/dashboard"
+                className={buttonVariants({
+                  variant: "ghost",
+                  size: "sm",
+                })}
+              >
+                Dashboard
+              </Link>
+
+              <UserAccountNav
+                name={
+                  !user.given_name || !user.family_name
+                  ? "Your Account"
+                  : `${user.given_name} ${user.family_name}`
+                }
+                email={user.email ?? ""}
+                imageUrl={user.picture ?? ""}
+              />
+            </>}
           </div>
         </div>
       </MaxWidthWrapper>
     </nav>
-  );
-};
+  )
+}
 
-export default Navbar;
+export default Navbar
