25aa9248d67051d2c95a877dec7c049c13f51746
schandillia <amit@schandillia.com>
2023-12-30 08:42:04 +0530

    Removed semicolons

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,10 +1,10 @@
-"use client";
+"use client"
 
-import { trpc } from "@/app/_trpc/client";
+import { trpc } from "@/app/_trpc/client"
 // import { absoluteUrl } from '@/lib/utils'
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { httpBatchLink } from "@trpc/client";
-import { PropsWithChildren, useState } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { httpBatchLink } from "@trpc/client"
+import { PropsWithChildren, useState } from "react"
 
 const Providers = ({ children }: PropsWithChildren) => {
   const [queryClient] = useState(() => new QueryClient());
@@ -16,13 +16,13 @@ const Providers = ({ children }: PropsWithChildren) => {
         }),
       ],
     }),
-  );
+  )
 
   return (
     <trpc.Provider client={trpcClient} queryClient={queryClient}>
       <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
     </trpc.Provider>
-  );
-};
+  )
+}
 
-export default Providers;
+export default Providers
