19e1effa8403a19e6e72778c8e4df1dc09b26a75
schandillia <amit@schandillia.com>
2024-01-07 19:11:08 +0530

    Minor fix to Tailwind breakpoints

--- src/components/TitleCaseInput.tsx
+++ src/components/TitleCaseInput.tsx
@@ -143,7 +143,7 @@ function TitleCaseInput() {
             <TooltipProvider>
               <RadioGroup
                 value={selectedStyle}
-                className="grid xs:grid-cols-2 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-8 gap-4"
+                className="grid sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-8 gap-4"
               >
                 {styles.map((style, index) => (
                   <div key={style} className="col-span-1 space-x-2">
