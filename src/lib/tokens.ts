f402ae5aa6c6e9baaf52cbb5603ebd5ab19d0364
schandillia <amit@schandillia.com>
2024-01-11 22:28:35 +0530

    [FEAT] set OTP expiration to 5 minutes

--- src/lib/tokens.ts
+++ src/lib/tokens.ts
@@ -8,7 +8,7 @@ import { db } from "@/lib/db"
 
 export const generateTwoFactorToken = async (email: string) => {
   const token = crypto.randomInt(100_000, 1_000_000).toString()
-  const expires = new Date(new Date().getTime() + 600 * 1000) // Expire token in 10 minutes
+  const expires = new Date(new Date().getTime() + 5 * 60 * 1000) // Expire token in 5 minutes
   const existingToken = await getTwoFactorTokenByEmail(email)
   if (existingToken) {
     await db.twoFactorToken.delete({
