2719f0b879316172e6254f5f89090a98e7bb83c6
schandillia <amit@schandillia.com>
2024-01-15 16:56:57 +0530

    [FEAT] implement PDF controls in PaperViewer, including focus and fullscreen modes

---
+++ src/components/research/PaperFullscreen.tsx
@@ -0,0 +1,71 @@
+/* eslint-disable @typescript-eslint/no-unused-vars, @typescript-eslint/no-shadow, react/no-array-index-key */
+
+import React, { useState, useCallback } from "react"
+import { Expand, Loader } from "lucide-react"
+import SimpleBar from "simplebar-react"
+import { Document, Page } from "react-pdf"
+import { useResizeDetector } from "react-resize-detector"
+
+import { FullScreen, useFullScreenHandle } from "react-full-screen"
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog"
+import { useToast } from "@/components/ui/use-toast"
+import { Button } from "@/components/ui/button"
+
+interface PdfFullscreenProps {
+  file: any
+}
+
+function PdfFullscreen({ file }: PdfFullscreenProps) {
+  const [isOpen, setIsOpen] = useState(false)
+  const [numPages, setNumPages] = useState<number>()
+
+  const { toast } = useToast()
+
+  const { width, ref } = useResizeDetector()
+
+  const handle = useFullScreenHandle()
+
+  return (
+    <>
+      <Button
+        onClick={handle.enter}
+        variant="ghost"
+        className="gap-1.5"
+        aria-label="fullscreen"
+      >
+        <Expand className="h-4 w-4" />
+      </Button>
+      <FullScreen handle={handle}>
+        {handle.active ? (
+          <SimpleBar autoHide className="max-h-full">
+            <div ref={ref}>
+              <Document
+                loading={
+                  <div className="flex justify-center">
+                    <Loader className="my-24 h-6 w-6 text-green-600 animate-spin" />
+                  </div>
+                }
+                onLoadError={() => {
+                  toast({
+                    title: "Error loading PDF",
+                    description: "Please try again later",
+                    variant: "destructive",
+                  })
+                }}
+                onLoadSuccess={({ numPages }) => setNumPages(numPages)}
+                file={file}
+                className="max-h-full"
+              >
+                {new Array(numPages).fill(0).map((_, i) => (
+                  <Page key={i} width={width || 1} pageNumber={i + 1} />
+                ))}
+              </Document>
+            </div>
+          </SimpleBar>
+        ) : null}
+      </FullScreen>
+    </>
+  )
+}
+
+export default PdfFullscreen
--- src/components/research/PaperViewer.tsx
+++ src/components/research/PaperViewer.tsx
@@ -1,50 +1,244 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-shadow, react/jsx-props-no-spreading, @typescript-eslint/no-unused-vars */
 
+"use client"
+
+import {
+  ChevronLeft,
+  ChevronRight,
+  Loader,
+  RotateCw,
+  RotateCcw,
+  ZoomIn,
+  ZoomOut,
+} from "lucide-react"
 import { Document, Page, pdfjs } from "react-pdf"
-import { useState, useEffect } from "react"
+
+import "react-pdf/dist/Page/AnnotationLayer.css"
+import "react-pdf/dist/Page/TextLayer.css"
+
+import { useResizeDetector } from "react-resize-detector"
+import { useState } from "react"
+
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import SimpleBar from "simplebar-react"
+import { cn } from "@/lib/utils"
+
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+import { useToast } from "@/components/ui/use-toast"
+import PaperFullscreen from "@/components/research/PaperFullscreen"
+import PaperFocus from "@/components/research/PaperFocus"
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`
 
-interface PaperViewerProps {
+interface PdfRendererProps {
   file: any
 }
 
-function PaperViewer({ file }: PaperViewerProps) {
-  const [numPages, setNumPages] = useState(null)
-  const [pageNumber, setPageNumber] = useState(1)
-  const [arrayBuffer, setArrayBuffer] = useState<ArrayBuffer | null>(null)
+function PdfRenderer({ file }: PdfRendererProps) {
+  const { toast } = useToast()
 
-  useEffect(() => {
-    // Ensure file is a valid PDF
-    if (!file || file.type !== "application/pdf") {
-      return // Handle invalid file
-    }
+  const [numPages, setNumPages] = useState<number>()
+  const [currPage, setCurrPage] = useState<number>(1)
+  const [scale, setScale] = useState<number>(1)
+  const [rotation, setRotation] = useState<number>(0)
+  const [renderedScale, setRenderedScale] = useState<number | null>(null)
 
-    const fileReader = new FileReader()
-    fileReader.onload = (event) => {
-      const receivedArrayBuffer = event.target?.result
-      setArrayBuffer(receivedArrayBuffer as ArrayBuffer)
-    }
-    fileReader.readAsArrayBuffer(file)
-  }, [file])
+  const isLoading = renderedScale !== scale
+
+  const CustomPageValidator = z.object({
+    page: z
+      .string()
+      .refine((num) => Number(num) > 0 && Number(num) <= numPages!),
+  })
+
+  type TCustomPageValidator = z.infer<typeof CustomPageValidator>
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setValue,
+  } = useForm<TCustomPageValidator>({
+    defaultValues: {
+      page: "1",
+    },
+    resolver: zodResolver(CustomPageValidator),
+  })
+
+  const { width, ref } = useResizeDetector()
+
+  const handlePageSubmit = ({ page }: TCustomPageValidator) => {
+    setCurrPage(Number(page))
+    setValue("page", String(page))
+  }
+
+  const normalizeRotation = (rotation: any) => {
+    // Use the modulo operator to keep the value within 0-360 degrees
+    const normalizedRotation = ((rotation % 360) + 360) % 360
+
+    // Map the normalized value to the expected range (0, 90, 180, 270)
+    return Math.round(normalizedRotation / 90) * 90
+  }
 
   return (
-    <div>
-      {arrayBuffer && (
-        <div className="w-full rounded-md shadow flex flex-col items-center">
-          <div className="flex-1 w-full max-h-screen">
-            <Document file={{ data: arrayBuffer }}>
+    <div className="w-full rounded-md shadow flex flex-col items-center">
+      <div className="h-14 w-full border-b border-gray-300 dark:border-gray-500 flex items-center justify-between px-2">
+        <div className="flex items-center gap-1.5">
+          <Button
+            disabled={currPage <= 1}
+            onClick={() => {
+              setCurrPage((prev) => (prev - 1 > 1 ? prev - 1 : 1))
+              setValue("page", String(currPage - 1))
+            }}
+            variant="ghost"
+            aria-label="previous page"
+          >
+            <ChevronLeft className="h-4 w-4" />
+          </Button>
+
+          <div className="flex items-center gap-1.5">
+            <Input
+              {...register("page")}
+              className={cn(
+                "w-12 h-8",
+                errors.page && "focus-visible:ring-red-500",
+              )}
+              onKeyDown={(e) => {
+                if (e.key === "Enter") {
+                  handleSubmit(handlePageSubmit)()
+                }
+              }}
+            />
+            <p className="text-gray-600 dark:text-gray-400 text-sm space-x-1">
+              <span>/</span>
+              <span>{numPages ?? "x"}</span>
+            </p>
+          </div>
+
+          <Button
+            disabled={numPages === undefined || currPage === numPages}
+            onClick={() => {
+              setCurrPage((prev) =>
+                prev + 1 > numPages! ? numPages! : prev + 1,
+              )
+              setValue("page", String(currPage + 1))
+            }}
+            variant="ghost"
+            aria-label="next page"
+          >
+            <ChevronRight className="h-4 w-4" />
+          </Button>
+        </div>
+
+        <div className="flex items-center gap-2">
+          {/* Zoom out */}
+          <Button
+            disabled={scale === 1}
+            variant="ghost"
+            onClick={() => {
+              setScale(scale - 0.25)
+            }}
+          >
+            <ZoomOut className="h-4 w-4" />
+          </Button>
+          <span className="text-sm text-gray-600 dark:text-gray-400">
+            {scale * 100}%
+          </span>
+          {/* Zoom in */}
+          <Button
+            variant="ghost"
+            onClick={() => {
+              setScale(scale + 0.25)
+            }}
+          >
+            <ZoomIn className="h-4 w-4" />
+          </Button>
+          <Button
+            className="ml-2 uppercase"
+            variant="secondary"
+            onClick={() => {
+              setScale(1)
+            }}
+          >
+            reset
+          </Button>
+        </div>
+
+        <div className="flex gap-0">
+          <Button
+            onClick={() => setRotation((prev) => normalizeRotation(prev - 90))}
+            variant="ghost"
+            aria-label="rotate 90 degrees"
+          >
+            <RotateCcw className="h-4 w-4" />
+          </Button>
+          <Button
+            onClick={() => setRotation((prev) => normalizeRotation(prev + 90))}
+            variant="ghost"
+            aria-label="rotate 90 degrees"
+          >
+            <RotateCw className="h-4 w-4" />
+          </Button>
+
+          <PaperFocus file={file} />
+          <PaperFullscreen file={file} />
+        </div>
+      </div>
+
+      <div className="flex-1 w-full max-h-screen">
+        <SimpleBar autoHide className="max-h-[calc(100vh-10rem)]">
+          <div ref={ref}>
+            <Document
+              loading={
+                <div className="flex justify-center">
+                  <Loader className="my-24 h-6 w-6 text-green-600 animate-spin" />
+                </div>
+              }
+              onLoadError={() => {
+                toast({
+                  title: "Error loading document",
+                  description: "Please try again later",
+                  variant: "destructive",
+                })
+              }}
+              onLoadSuccess={({ numPages }) => setNumPages(numPages)}
+              file={file}
+              className="max-h-full"
+            >
+              {isLoading && renderedScale ? (
+                <Page
+                  width={width || 1}
+                  pageNumber={currPage}
+                  scale={scale}
+                  rotate={rotation}
+                  key={`@${renderedScale}`}
+                />
+              ) : null}
+
               <Page
-                pageNumber={1}
-                renderTextLayer={false}
-                renderAnnotationLayer={false}
+                className={cn(isLoading ? "hidden" : "")}
+                width={width || 1}
+                pageNumber={currPage}
+                scale={scale}
+                rotate={rotation}
+                key={`@${scale}`}
+                loading={
+                  <div className="flex justify-center">
+                    <Loader className="my-24 h-6 w-6 text-green-600 animate-spin" />
+                  </div>
+                }
+                onRenderSuccess={() => setRenderedScale(scale)}
               />
             </Document>
           </div>
-        </div>
-      )}
+        </SimpleBar>
+      </div>
     </div>
   )
 }
 
-export default PaperViewer
+export default PdfRenderer
