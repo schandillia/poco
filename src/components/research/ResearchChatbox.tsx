8b4ba2aca1794c65fac412fa7a918a0cea060e3f
schandillia <amit@schandillia.com>
2024-01-18 15:01:47 +0530

    [FEAT] improve research chatbox logic

--- src/components/research/ResearchChatbox.tsx
+++ src/components/research/ResearchChatbox.tsx
@@ -5,13 +5,14 @@
 import { useEffect, useState } from "react"
 import { Icons } from "@/components/Icons"
 import ResearchInput from "@/components/research/ResearchInput"
-import { useToast } from "@/components/ui/use-toast"
 
 interface ResearchChatboxProps {
   file: File
 }
 function ResearchChatbox({ file }: ResearchChatboxProps) {
-  const { toast } = useToast()
+  const [isVectorized, setIsVectorized] = useState(false)
+  const [isPaperError, setIsPaperError] = useState(false)
+
   const handleVectorize = async (givenFile: File) => {
     try {
       const res = await fetch("/api/vectorize", {
@@ -20,28 +21,38 @@ function ResearchChatbox({ file }: ResearchChatboxProps) {
       })
       if (!res.ok) throw new Error(await res.text())
       console.log(await res.json())
+      setIsVectorized(true)
     } catch (error: any) {
-      toast({
-        title: "Error processing your paper",
-        description: "Please try again later",
-        variant: "destructive",
-      })
+      setIsPaperError(true)
     }
   }
-  if (file) handleVectorize(file)
+  useEffect(() => {
+    if (file) handleVectorize(file)
+  }, [])
 
   return (
     <div className="relative min-h-full bg-gray-50 dark:bg-zinc-800 flex divide-y divide-gray-300 dark:divide-gray-500 flex-col justify-between gap-2">
       <div className="flex-1 flex justify-center items-center flex-col mb-28">
-        <div className="flex flex-col items-center gap-2">
-          <Icons.Loader className="h-8 w-8 text-green-600 animate-spin" />
-          <h3 className="font-semibold text-xl">Preparing your paper</h3>
-          <p className="text-gray-600 dark:text-gray-400 text-sm">
-            This shouldn’t take long.
-          </p>
-        </div>
+        {isPaperError && (
+          <div className="flex flex-col items-center gap-2">
+            <Icons.Alert className="h-8 w-8 text-destructive" />
+            <h3 className="font-semibold text-xl">Something went wrong!</h3>
+            <p className="text-gray-600 dark:text-gray-400 text-sm">
+              There was an error preparing your paper. Please try again later.
+            </p>
+          </div>
+        )}
+        {!isVectorized && !isPaperError && (
+          <div className="flex flex-col items-center gap-2">
+            <Icons.Loader className="h-8 w-8 text-green-600 animate-spin" />
+            <h3 className="font-semibold text-xl">Preparing your paper...</h3>
+            <p className="text-gray-600 dark:text-gray-400 text-sm">
+              This shouldn’t take long.
+            </p>
+          </div>
+        )}
       </div>
-      <ResearchInput />
+      {isVectorized && !isPaperError && <ResearchInput />}
     </div>
   )
 }
