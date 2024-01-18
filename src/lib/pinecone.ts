35f42661b19a2eb20f49ba5d3e578184d63bd1d9
schandillia <amit@schandillia.com>
2024-01-18 14:29:30 +0530

    [FEAT] update pinecone.ts to newer code

--- src/lib/pinecone.ts
+++ src/lib/pinecone.ts
@@ -1,13 +1,21 @@
-import { PineconeClient } from "@pinecone-database/pinecone"
+// import { PineconeClient } from "@pinecone-database/pinecone"
 
-const getPineconeClient = async () => {
-  const client = new PineconeClient()
+// const getPineconeClient = async () => {
+//   const client = new PineconeClient()
 
-  await client.init({
-    apiKey: process.env.PINECONE_API_KEY!,
-    environment: process.env.PINECONE_ENVIRONMENT as string,
-  })
+//   await client.init({
+//     apiKey: process.env.PINECONE_API_KEY!,
+//     environment: process.env.PINECONE_ENVIRONMENT as string,
+//   })
 
-  return client
-}
+//   return client
+// }
+// export default getPineconeClient
+
+import { Pinecone } from "@pinecone-database/pinecone"
+
+const getPineconeClient = new Pinecone({
+  apiKey: process.env.PINECONE_API_KEY!,
+  environment: process.env.PINECONE_ENVIRONMENT as string,
+})
 export default getPineconeClient
