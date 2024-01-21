91a6ca603a4815c81418bf7bd7381d793382df44
schandillia <amit@schandillia.com>
2024-01-21 08:32:04 +0530

    [FIX] make minor changes

--- src/components/research/ResearchSection.tsx
+++ src/components/research/ResearchSection.tsx
@@ -5,7 +5,7 @@ import { Icons } from "@/components/Icons"
 function ResearchSection() {
   return (
     <div className="relative min-h-full bg-gray-50 dark:bg-zinc-800 flex divide-y divide-gray-300 dark:divide-gray-500 flex-col justify-between gap-2">
-      <div className="flex-1 flex justify-center items-center flex-col mb-28">
+      <div className="flex-1 flex justify-center items-center flex-col">
         <div className="flex flex-col items-center gap-2">
           <Icons.Loader className="h-8 w-8 text-green-600 animate-spin" />
           <h3 className="font-semibold text-xl">Waiting for paper...</h3>
